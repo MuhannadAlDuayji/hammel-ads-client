de55db066119eebb2961ad3e7ae10270c5af6796
Firas <mellitifiras@protonmail.com>
2023-03-27 21:15:40 +0100

    translating the campaign table options

--- src/main/dashboard/pages/campaign/components/CampaignTableRow.tsx
+++ src/main/dashboard/pages/campaign/components/CampaignTableRow.tsx
@@ -181,7 +181,7 @@ export default function CampaignTableRow({ campaign }: CampaignsTableRowProps) {
                 <Menu as="div" className="relative inline-block text-left">
                     <div>
                         <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                            Options
+                            {t("options")}
                             <ChevronDownIcon
                                 className="-mr-1 h-5 w-5 text-gray-400"
                                 aria-hidden="true"
@@ -198,7 +198,7 @@ export default function CampaignTableRow({ campaign }: CampaignsTableRowProps) {
                         leaveFrom="transform opacity-100 scale-100"
                         leaveTo="transform opacity-0 scale-95"
                     >
-                        <Menu.Items className="absolute right-0 z-10 mt-2 w-24 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <Menu.Items className="absolute left-0  z-10 m-2 w-24 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                             <div className="py-1">
                                 <Menu.Item>
                                     {({ active }) => (
@@ -211,7 +211,7 @@ export default function CampaignTableRow({ campaign }: CampaignsTableRowProps) {
                                                 "block px-4 py-2 text-sm w-full text-left"
                                             )}
                                         >
-                                            edit
+                                            {t("edit")}
                                         </button>
                                     )}
                                 </Menu.Item>
@@ -241,7 +241,7 @@ export default function CampaignTableRow({ campaign }: CampaignsTableRowProps) {
                                                         "block px-4 py-2 text-sm text-orange-700 w-full text-left"
                                                     )}
                                                 >
-                                                    stop
+                                                    {t("stop")}
                                                 </button>
                                             ) : (
                                                 <button
@@ -252,10 +252,10 @@ export default function CampaignTableRow({ campaign }: CampaignsTableRowProps) {
                                                         active
                                                             ? "bg-gray-100 text-gray-900"
                                                             : "text-gray-700",
-                                                        "block px-4 py-2 text-sm text-orange-700 w-full text-left"
+                                                        "block px-4 py-2 text-sm text-green-600 w-full text-left"
                                                     )}
                                                 >
-                                                    resume
+                                                    {t("resume")}
                                                 </button>
                                             );
                                         }}
