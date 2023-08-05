fd51067f79f9f33388c001b0b4d80dfb1c2d4192
Firas Melliti <mellitifiras@protonmail.com>
2023-08-05 06:14:47 +0100

    adding select all regions feature

--- src/main/dashboard/pages/campaign/EditCampaignPage.tsx
+++ src/main/dashboard/pages/campaign/EditCampaignPage.tsx
@@ -138,6 +138,12 @@ function EditCampaignPage({}: Props) {
             });
         }
     };
+    const handleAddAllCities = () => {
+        setCampaignInfo({
+            ...campaignInfo,
+            targetedCities: [...citiesList],
+        });
+    };
     const handleRemoveCity = (city: string) => {
         setCampaignInfo({
             ...campaignInfo,
@@ -217,7 +223,6 @@ function EditCampaignPage({}: Props) {
             campaignInfo.targetedCities.length === 0 &&
             country !== "All Countries"
         ) {
-            console.log(campaignInfo.country);
             const message = t("no_cities_message");
             setErrorMessage(message);
             return false;
@@ -281,7 +286,6 @@ function EditCampaignPage({}: Props) {
         const country = countryList.find(
             (country) => country.name === campaignInfo.country
         )?.value;
-        console.log(country, campaign.country);
         if (country?.toLowerCase() !== campaign.country.toLowerCase()) {
             setCampaignInfo({
                 ...campaignInfo,
@@ -492,16 +496,29 @@ function EditCampaignPage({}: Props) {
                                         <br />
                                         <div className="mt-1 sm:col-span-2 sm:mt-0">
                                             <div className="block w-full max-w-lg">
+                                                <button
+                                                    onClick={handleAddAllCities}
+                                                    className={`rounded-md border-2 px-2 py-1 mb-5 text-md ${
+                                                        campaignInfo
+                                                            .targetedCities
+                                                            .length ===
+                                                        citiesList.length
+                                                            ? "bg-green-100 border-2 border-green-600"
+                                                            : "border-2 border-gray-200"
+                                                    }`}
+                                                >
+                                                    {t("select_all_locations")}
+                                                </button>
                                                 <ul className="flex gap-1  w-full max-w-lg flex-wrap">
                                                     {citiesList.map(
                                                         (city, i) => (
-                                                            <>
+                                                            <div key={i}>
                                                                 {!campaignInfo.targetedCities.includes(
                                                                     city
                                                                 ) ? (
                                                                     <li
                                                                         key={i}
-                                                                        className=" bg-gray-100 flex rounded-lg flex-row gap-2 items-center pr-2"
+                                                                        className=" bg-gray-100 flex rounded-lg flex-row gap-2 items-center pr-2 border-2 border-gray-200"
                                                                     >
                                                                         <p className="m-0 p-0">
                                                                             {t(
@@ -523,7 +540,7 @@ function EditCampaignPage({}: Props) {
                                                                 ) : (
                                                                     <li
                                                                         key={i}
-                                                                        className="bg-green-100 flex rounded-lg flex-row gap-2 items-center pr-2 border-2 border-green-600"
+                                                                        className="flex rounded-lg flex-row gap-2 items-center pr-2 bg-green-100 border-2 border-green-600"
                                                                     >
                                                                         <p className="m-0 p-0">
                                                                             {t(
@@ -543,7 +560,7 @@ function EditCampaignPage({}: Props) {
                                                                         </button>
                                                                     </li>
                                                                 )}
-                                                            </>
+                                                            </div>
                                                         )
                                                     )}
                                                 </ul>
